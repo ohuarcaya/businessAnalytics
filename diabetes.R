d4ab2575cc010d1e95181d49c93adbc304899454
Oscar Huarcaya <ohuarcayac@uni.pe>
2018-09-02 23:55:24 -0500

    r code

--- diabetes.R
+++ diabetes.R
@@ -1,11 +1,13 @@
+install.packages(c('repr', 'IRdisplay', 'evaluate', 'crayon', 'pbdZMQ', 'devtools', 'uuid', 'digest'))
 install.packages(c("rpart"), dependencies = c("Depends", "Suggests"))
 install.packages("rattle")
 install.packages("cluster")
 install.packages("foreign")
 install.packages("caret")
+install.packages('devtools')
 library(rpart)
 #########################################################
-#  Algoritmo RPART PARA ARBOL DE DECISIÓN               #
+#  Algoritmo RPART PARA ARBOL DE DECISI?N               #
 #########################################################
 
 diabetes<-read.table(file.choose(),header=T,sep=",") 
@@ -26,22 +28,22 @@ arbolb=rpart(diagnostic~glucose+sspg,data=diabetes,method="class")
 arbolb
 drawTreeNodes(arbolb)
 
-#Predicción usando el árbol a
+#Predicci?n usando el ?rbol a
 #Calcular los valores predichos
 pred<-predict(arbola,diabetes[,c(1,2)],type="class")
 error=mean(pred!=diabetes$diagnostic)
 error
 
-#Calcular la matriz de confusión
+#Calcular la matriz de confusi?n
 table(pred,diabetes$diagnostic)
 
-#Predicción usando el árbol b
+#Predicci?n usando el ?rbol b
 #Calcular los valores predichos
 pred<-predict(arbolb,diabetes[,c(1,3)],type="class")
 error=mean(pred!=diabetes$diagnostic)
 error
 
-#Calcular la matriz de confusión
+#Calcular la matriz de confusi?n
 t <- table(pred,diabetes$diagnostic)
 t
 plot(t)
